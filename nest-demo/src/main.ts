7fdec198816ffe68b1aa12ad57079c5b2a78e083
Vaibhav Patil <melayer.vaibhav@gmail.com>
2018-11-16 18:41:12 +0530

    Cross-origin mechanism is implemented

--- nest-demo/src/main.ts
+++ nest-demo/src/main.ts
@@ -1,7 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
 
 declare const module: any;
 
@@ -9,10 +8,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule , {
     logger: console,
   });
-  app.use(cors());
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
-  // app.use(cors());
 
   if (module.hot) {
     module.hot.accept();
